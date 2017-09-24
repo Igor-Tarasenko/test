42492f4cde41862bbf498b6eb05b28494478249e
Igor-Tarasenko <31444859+Igor-Tarasenko@users.noreply.github.com>
2017-09-24 16:56:49 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,35 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', function() {
     const table = new Table(document.querySelector('.block'));
-    document.getElementById('table').addEventListener('mouseover', function(cell) {
-        const styleRowBtn = document.querySelector('button.delete-row');
-        styleRowBtn.style.left = 'cell.offsetTop' + 'px';
+    const styleRowBtn = document.querySelector('button.delete-row');
+    const styleColBtn = document.querySelector('button.delete-column')
+    document.getElementById('table').addEventListener('mouseover', function() {
+        styleRowBtn.style.background = '#d20505';
+        styleRowBtn.style.opacity = '1';
+        styleRowBtn.style.transition = '.4s'
+        styleColBtn.style.opacity = '1';
+        styleColBtn.style.background = '#d20505';
+        styleColBtn.style.transition = '.4s'
+    });
+    styleRowBtn.addEventListener('mouseover', function() {
+        styleRowBtn.style.opacity = '1';
+        styleRowBtn.style.background = '#d45f68';
+        styleRowBtn.style.transition = '.6s';
+    });
+    styleColBtn.addEventListener('mouseover', function() {
+        styleColBtn.style.opacity = '1';
+        styleColBtn.style.background = '#d45f68';
+        styleColBtn.style.transition = '.6s';
+    });
+    styleRowBtn.addEventListener('mouseleave', function() {
+        styleRowBtn.style.opacity = '0';
+    });
+    styleColBtn.addEventListener('mouseleave', function() {
+        styleColBtn.style.opacity = '0';
+    });
+    document.getElementById('table').addEventListener('mouseleave', function() {
+        styleRowBtn.style.opacity = '0';
+        styleColBtn.style.opacity = '0';
     });
 });
 
@@ -17,13 +43,13 @@ class Table {
         this.rowCount = this.rowCount.bind(this);
         this.delRow = this.delRow.bind(this);
         this.delColumn = this.delColumn.bind(this);
-        this.mouseOverHandler = this.mouseOverHandler.bind(this);
+        // this.mouseOver = this.mouseOver.bind(this);
 
         this.field.querySelector('button.add-column').onclick = this.addCol;
         this.field.querySelector('button.add-row').onclick = this.addRow;
         this.field.querySelector('button.delete-row').onclick = this.delRow;
         this.field.querySelector('button.delete-column').onclick = this.delColumn;
-        this.table.addEventListener('mouseover', this.mouseOverHandler);
+        // this.table.addEventListener('mouseover', this.mouseOver);
     }
 
     addRow() {
@@ -65,13 +91,11 @@ class Table {
     }
 
     delRow () {
-        // let i = r.parentNode.parentNode.rowIndex;
         this.table.deleteRow(-1);
         this.rowCount();
     }
 
     delColumn () {
-        // var index = r.parentNode.parentNode.cellIndex;
         let rows = this.table.tBodies[0].rows;
         for (let i = 0, l = rows.length; i < l; i++) {
             let dellCell = rows[i].deleteCell(-1);
@@ -79,15 +103,15 @@ class Table {
         this.columnCount ();
     }
 
-    mouseOverHandler () {
-        if (event.target instanceof HTMLTableCellElement) {
-            const cell = event.target;
-            // this.moveColRemoveButton(cell);
-            this.moveRowRemoveButton(cell);
-        }
-    }
-
-    moveRowRemoveButton(cell) {
-        this.field.querySelector('button.delete-row').style.top = 'cell.offsetTop' + 'px';
-    }
+    // mouseOver () {
+    //     if (event.target instanceof HTMLTableCellElement) {
+    //         const cell = event.target;
+    //         this.moveColButton(cell);
+    //         this.moveRowButton(cell);
+    //     }
+    // }
+    //
+    // moveRowRemoveButton(cell) {
+    //     this.field.querySelector('button.delete-row').style.top = 'cell.offsetTop' + 'px';
+    // }
 }
